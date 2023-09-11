1444b4b7037611b5362ed1d30b5ef281d6dd9360
Saagie-Copilot <140416768+Saagie-Copilot@users.noreply.github.com>
2023-09-11 14:33:00 +0200

    Add new job test
    Add new job test

---
+++ code/jobs/test_data/__main__.py
@@ -0,0 +1,15 @@
+# create a function to generate data
+def generate_data():
+    # create a list of 1000 random numbers
+    data = [random.randint(0, 1000) for i in range(1000)]
+    # return the data
+    return data
+
+# create a function to generate english names
+def generate_names():
+    # create a list of 1000 random names
+    names = [fake.name() for i in range(1000)]
+    # return the names
+    return names
+
+print(generate_names())

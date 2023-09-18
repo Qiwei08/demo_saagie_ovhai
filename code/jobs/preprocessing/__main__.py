acb63141390b12f3a93fc73042be026ed7ace2c8
github.com <github-copilot@saagie.com>
2023-09-18 08:40:12 +0000

    New preprocessing

--- code/jobs/preprocessing/__main__.py
+++ code/jobs/preprocessing/__main__.py
@@ -3,70 +3,71 @@
 # rename the output with the following pattern "<timestamp>_extract_comment_IMDB_from_HF.csv.
 # Display a message  before starting processing and when it's finished plus the total duration of the operation
 
+
 import boto3
-import re
-import time
 import pandas as pd
+from bs4 import BeautifulSoup
+import re
+import datetime
+import time
 
-# Function to remove HTML tags from a string
+# Function to remove HTML tags from text
 def remove_html_tags(text):
-    clean = re.compile('<.*?>')
-    return re.sub(clean, '', text)
+    soup = BeautifulSoup(text, "html.parser")
+    return soup.get_text()
 
-# Function to remove emojis from a string
+# Function to remove emojis from text
 def remove_emojis(text):
     emoji_pattern = re.compile("["
                                u"\U0001F600-\U0001F64F"  # emoticons
                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                               u"\U00002500-\U00002BEF"  # chinese char
-                               u"\U00002702-\U000027B0"
                                u"\U00002702-\U000027B0"
                                u"\U000024C2-\U0001F251"
-                               u"\U0001f926-\U0001f937"
-                               u"\U00010000-\U0010ffff"
-                               u"\u2640-\u2642"
-                               u"\u2600-\u2B55"
-                               u"\u200d"
-                               u"\u23cf"
-                               u"\u23e9"
-                               u"\u231a"
-                               u"\ufe0f"  # dingbats
-                               u"\u3030"
                                "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', text)
 
-# Function to copy the dataset to S3 bucket and perform required operations
-def process_dataset():
-    # Start time
-    start_time = time.time()
+# Function to copy dataset from Huggingface to S3 bucket
+def copy_dataset_to_s3():
+    # Copying dataset code here
+    pass
 
-    # Display message before starting processing
-    print("Starting dataset processing...")
-
-    # Copy the dataset to S3 bucket
-    s3 = boto3.client('s3')
-    s3.download_file('huggingface-datasets', 'tiny-bert-dataset.csv', 'dataset.csv')
-
+# Function to process the dataset
+def process_dataset(dataset_path):
     # Read the dataset into a pandas DataFrame
-    df = pd.read_csv('dataset.csv')
+    df = pd.read_csv(dataset_path)
 
     # Remove HTML tags and emojis from the 'comment' column
     df['comment'] = df['comment'].apply(remove_html_tags)
     df['comment'] = df['comment'].apply(remove_emojis)
 
-    # Rename the output file with the specified pattern
-    timestamp = time.strftime("%Y%m%d%H%M%S")
+    # Generate timestamp for renaming output file
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Rename the output file
     output_filename = f"{timestamp}_extract_comment_IMDB_from_HF.csv"
     df.to_csv(output_filename, index=False)
 
-    # Display message after finishing processing
-    print("Dataset processing completed!")
+    return output_filename
 
-    # Calculate total duration of the operation
-    duration = time.time() - start_time
-    print(f"Total duration: {duration} seconds")
+# Start of the program
+start_time = time.time()
 
-# Call the function to process the dataset
-process_dataset()
+# Display a message before starting processing
+print("Starting dataset processing...")
+
+# Copy dataset from Huggingface to S3 bucket
+copy_dataset_to_s3()
+
+# Process the dataset and get the output filename
+output_filename = process_dataset("path/to/dataset.csv")
+
+# Display a message when finished processing
+print(f"Dataset processing finished. Output file: {output_filename}")
+
+# Calculate and display the total duration of the operation
+end_time = time.time()
+duration = end_time - start_time
+print(f"Total duration: {duration} seconds")
+

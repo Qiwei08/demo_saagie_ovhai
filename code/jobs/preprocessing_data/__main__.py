8a62b0f6734736b94c46e3232b28f71e1fe40c85
github.com <github-copilot@saagie.com>
2023-09-19 09:04:38 +0000

    add generated code

--- code/jobs/preprocessing_data/__main__.py
+++ code/jobs/preprocessing_data/__main__.py
@@ -4,4 +4,52 @@
 # Display a message  before starting processing and when it's finished plus the total duration of the operation
 
 
-print("Start preprocessing data")
+import boto3
+import datetime
+import pandas as pd
+import re
+import requests
+
+def copy_dataset_to_s3():
+    # Display a message before starting processing
+    print("Copying dataset to S3 bucket...")
+
+    # Get the current timestamp
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Download the dataset from Huggingface
+    url = "https://path/to/dataset.csv"
+    response = requests.get(url)
+    dataset = response.text
+
+    # Remove HTML tags using regular expressions
+    dataset = re.sub("<.*?>", "", dataset)
+
+    # Remove emojis using regular expressions
+    dataset = re.sub("[^\u0000-\uFFFF]", "", dataset)
+
+    # Save the processed dataset to a file
+    output_filename = f"{timestamp}_extract_comment_IMDB_from_HF.csv"
+    with open(output_filename, "w") as file:
+        file.write(dataset)
+
+    # Upload the file to S3 bucket
+    s3 = boto3.client("s3")
+    s3.upload_file(output_filename, "your-s3-bucket", output_filename)
+
+    # Display a message when finished
+    print("Dataset copied to S3 bucket successfully.")
+
+if __name__ == "__main__":
+    # Start measuring the time
+    start_time = datetime.datetime.now()
+
+    # Call the function to copy the dataset to S3 bucket
+    copy_dataset_to_s3()
+
+    # Calculate the total duration of the operation
+    end_time = datetime.datetime.now()
+    duration = end_time - start_time
+
+    # Display the total duration
+    print(f"Total duration: {duration}")

366e7371a9b20fdaf6369fd1fe8e4de8ee52d6a0
github.com <github-copilot@saagie.com>
2023-12-18 15:46:23 +0000

    update code of processing

--- code/jobs/preprocessing_data/__main__.py
+++ code/jobs/preprocessing_data/__main__.py
@@ -2,60 +2,72 @@
 # remove html balises and emoji 
 # rename the output with the following pattern "<timestamp>_extract_IMDB_comments_from_HF.csv"
 # Display a message  before starting processing and when it's finished plus the total duration of the operation
+# Display the number of rows and columns of the output file
+# Display the first 5 rows of the output file
 
 import time
+import pandas as pd
 import re
-import boto3
 from datasets import load_dataset
+import boto3
 from datetime import datetime
 
 # Function to remove HTML tags and Emojis from text
 def clean_text(text):
     # Remove HTML tags using regex
-    clean_html = re.sub(r'<.*?>', '', text)
-    # Remove Emojis and other unicode characters not in the ASCII range
-    clean_emoji = re.sub(r'[^\x00-\x7F]+', '', clean_html)
-    return clean_emoji
+    clean_html = re.compile('<.*?>')
+    text_without_html = re.sub(clean_html, '', text)
+    
+    # Remove Emojis
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    text_cleaned = emoji_pattern.sub(r'', text_without_html)
+    
+    return text_cleaned
 
-# Main function to process the dataset and upload it to S3
+# AWS S3 bucket details
+s3_bucket_name = 'your-s3-bucket-name'
+s3_client = boto3.client('s3')
+
 def main():
-    print("Starting the data processing...")
-
     start_time = time.time()
+    print("Starting the processing...")
 
     # Load dataset from Huggingface
-    dataset = load_dataset('imdb')
+    dataset = load_dataset("imdb")
 
-    # Clean the dataset by removing html tags and emojis
-    dataset_cleaned = dataset.map(lambda example: {'text': clean_text(example['text'])})
+    # Process the dataset: remove html balises and emojis
+    df = pd.DataFrame(dataset['train'])
+    df['text'] = df['text'].apply(clean_text)
 
-    # Convert to pandas dataframe
-    df = dataset_cleaned['train'].to_pandas()
-
-    # Get current timestamp
+    # Get current timestamp for file naming
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-
-    # Define the output filename
     output_filename = f"{timestamp}_extract_IMDB_comments_from_HF.csv"
 
-    # Save the dataframe to a CSV file
+    # Save dataframe to CSV
     df.to_csv(output_filename, index=False)
 
-    # Initialize boto3 S3 client
-    s3_client = boto3.client('s3')
-
-    # Define your bucket name here
-    bucket_name = 'your-bucket-name'
-
-    # Upload the CSV file to S3 bucket
-    s3_client.upload_file(output_filename, bucket_name, output_filename)
+    # Upload to S3 bucket
+    s3_client.upload_file(output_filename, s3_bucket_name, output_filename)
 
     end_time = time.time()
     duration = end_time - start_time
+    print(f"Finished processing. Total duration: {duration:.2f} seconds.")
+    
+    # Display number of rows and columns
+    num_rows, num_cols = df.shape
+    print(f"Number of rows: {num_rows}")
+    print(f"Number of columns: {num_cols}")
 
-    print(f"Data processing completed. The file has been saved to S3 as {output_filename}")
-    print(f"Total duration of operation: {duration:.2f} seconds.")
+    # Display the first 5 rows of the output file
+    print("First 5 rows of the output file:")
+    print(df.head())
 
-# Run the main function
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

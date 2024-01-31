a09802c1f74619722f8829f5b877919aaa10250e
github.com <github-copilot@saagie.com>
2024-01-31 13:59:56 +0000

    rollback

--- code/jobs/preprocessing_data/__main__.py
+++ code/jobs/preprocessing_data/__main__.py
@@ -1,4 +1,5 @@
 # Write a python program to copy a dataset for the tiny-bert model from Huggingface to S3 bucket,
-# remove html balises and emoji 
+# remove html tags and emojis 
 # rename the output with the following pattern "<timestamp>_extract_comment_IMDB_from_HF.csv.
 # Display a message  before starting processing and when it's finished plus the total duration of the operation
+
